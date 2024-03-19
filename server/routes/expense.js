2a1741a852440f1f3d5bc8ddc4a0a1b796de9094
Shantanu Kor <77278554+shantanu-kor@users.noreply.github.com>
2024-03-19 16:03:26 +0530

    Delete server directory

--- server/routes/expense.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const expenseController = require('../controllers/expense');
-
-const router = express.Router();
-
-router.post('/add-expense', expenseController.addExpense);
-
-module.exports = router;
